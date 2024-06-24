9234c7e322189d151bfa8c6676419e6e31eee30f
Royce Williams <royce@techsolvency.com>
2024-06-24 10:38:33 -0800

    run-with-logging: note where 'ts' can be found

--- run-with-logging.sh
+++ run-with-logging.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+#
+# 'ts' is usually pare of the 'moreutils' package.
 
 callattendant --config=app.cfg \
         | /bin/ts '[%Y-%m-%d %H:%M:%.S]' \
         | sudo /bin/tee -a /var/log/callattendant.log
+
